c4d820dd4bb17f496be6da3e09c374a8d321131a
Ricardo Amaral Filho <your-github-id+username@users.noreply.github.com>
2025-04-14 10:19:36 -0300

    ommiting

---
+++ ThirdParty/MAC_SDK/Source/Shared/Version.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/* build batch file */
+/*
+#ifdef APE_BATCH_FILE_VERSION
+Set _MA=1086
+Set _MAV=10.86
+#endif
+*/
+
+/* major version number */
+#define APE_VERSION_MAJOR 10
+
+/* build version number */
+#define APE_VERSION_REVISION 86
+#define APE_VERSION_REVISION_NUMBER 86
+
+/* library interface version, update this whenever the signature of an exported function changes */
+#define APE_INTERFACE_VERSION 11
+
+/* leave this so the end of file doesn't get truncated */
